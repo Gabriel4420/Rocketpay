25659f74c9a83e0f57644ee79e3c7c2942eeeb15
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2021-02-27 11:04:54 -0300

    operation and refactoring

--- lib/rocketpay_web/controllers/accounts_controller.ex
+++ lib/rocketpay_web/controllers/accounts_controller.ex
@@ -13,6 +13,15 @@ defmodule RocketpayWeb.AccountsController do
     end
 
   end
+
+  def withdraw(conn,params) do
+    with {:ok, %Account{} = account} <- Rocketpay.withdraw(params) do
+      conn
+      |> put_status(:ok)
+      |> render("update.json", account: account)
+    end
+
+  end
   #defp handle_response({:error, _result} = error, _conn), do: error
 
 end
